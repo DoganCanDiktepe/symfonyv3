c4706d85276b37225c5115b3f7ae318498725ea4
Doğancan Diktepe <ddiktepe@magiclick.com>
2017-10-31 16:36:54 +0300

    Symfony Demo Template

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony_v3
+==========
+
+A Symfony project created on October 24, 2017, 3:33 pm.
---
+++ app/AppCache.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AppCache extends HttpCache
+{
+
+}
